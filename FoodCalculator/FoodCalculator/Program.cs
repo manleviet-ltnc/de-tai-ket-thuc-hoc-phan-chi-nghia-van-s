35302c7c0c98b43538f033bd0f0fac7b013c850b
mmeanzz <31737212+mmeanzz@users.noreply.github.com>
2017-12-03 18:26:06 +0700

    Form mới

--- FoodCalculator/FoodCalculator/Program.cs
+++ FoodCalculator/FoodCalculator/Program.cs
@@ -16,7 +16,7 @@ namespace FoodCalculator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FoodCalculator());
         }
     }
 }
